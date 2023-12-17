428978d568434e4a14b4e8c9a350e9ba43560bec
Alex Nastev <alexnastev12@gmail.com>
2023-12-17 16:32:18 +0200

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using UnitConverterApp.MVVM.Views;
+
+namespace UnitConverterApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MenuView());
+        }
+    }
+}
